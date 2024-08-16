9052b3c8db98d1f63d7772924927333268b21b92
sdelrio <sdelrio@users.noreply.github.com>
2024-08-16 21:15:07 +0200

    add: pick-emoji script

--- scripts/pop-os-tune.sh
+++ scripts/pop-os-tune.sh
@@ -3,7 +3,7 @@
 # https://unix.stackexchange.com/questions/554908/disable-spectre-and-meltdown-mitigations
 #sudo kernelstub -a "mitigations=off"
 
-sudo apt -y install zsh
+sudo apt -y install zsh wl-clipboard
 
 [ $SHELL = /bin/zsh ] || chsh -s /usr/bin/zsh $USER
 

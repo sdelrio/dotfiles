d3c79911396611b9c776c8ccf7f7e3d6807c8884
sdelrio <sdelrio@users.noreply.github.com>
2024-08-16 22:14:49 +0200

    add: hide environments log when direnv is used

--- sync.sh
+++ sync.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
 
 # teller env > .config/fabric/.env
 
 # stow won't overwrite existing files
 test -L ~/.zshrc.sh || rm -f ~/.zshrc
+
+checkdir=(~/.local/bin
+  ~/.local/share/devbox/global/default
+  ~/.config/direnv)
+
+for mydir in "${checkdir[@]}"; do
+  echo ${mydir}
+  test -d ${mydir} || mkdir -p ${mydir}
+done
 
 # perl lang so it doesn't pop up when using es_ES.UTF-8 on first call
 export LC_ALL=C
